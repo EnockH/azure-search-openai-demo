c8234cb00b24e2606cb40ebf8163466f35ab3954
EnockH <133886819+EnockH@users.noreply.github.com>
2023-06-06 08:39:08 +0000

    Pending changes exported from your codespace

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What is work management?",
+        value: "What is work management?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Describe the operating model", value: "Describe the operating model" },
+    { text: "What are the objectives of the conveyor standard?", value: "What are the objectives of the conveyor standard?" }
 ];
 
 interface Props {
